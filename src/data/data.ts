5dd85533f7b868120f21bab31c1ddd390c97b7c3
alekskimeu <kimeualeks@gmail.com>
2025-02-21 20:45:42 +0300

    create what we do section

---
+++ src/data/data.ts
@@ -0,0 +1,22 @@
+import image from "../assets/hero.jpeg";
+
+export const activities = [
+  {
+    image,
+    title: "Educate",
+    description:
+      "We provide workshops, resources, and programs to inform and inspire our community. Through knowledge-sharing, we empower individuals to make informed decisions and create lasting change.",
+  },
+  {
+    image,
+    title: "Empower",
+    description:
+      "We believe in the power of community. By offering mentorship, skills training, and support networks, we help individuals build confidence and take action toward a brighter future.",
+  },
+  {
+    image,
+    title: "Elevate",
+    description:
+      "Together, we rise. Through collaboration, leadership development, and meaningful opportunities, we uplift our community and create pathways for success.",
+  },
+];
